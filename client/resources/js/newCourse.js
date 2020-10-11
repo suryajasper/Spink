1701e5defecd0a119f3966b5dce839e0ff530dcd
Surya Jasper <suryajasper2004@gmail.com>
2020-10-11 14:54:57 -0700

    display total time per day

--- client/resources/js/newCourse.js
+++ client/resources/js/newCourse.js
@@ -41,6 +41,25 @@ var selectedDays = [];
 
 var dayToTimes = {};
 
+dom('createCourseButton').disabled = true;
+
+function diff(start, end) {
+    start = start.value; //to update time value in each input bar
+    end = end.value; //to update time value in each input bar
+
+    start = start.split(":");
+    end = end.split(":");
+    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    var diff = endDate.getTime() - startDate.getTime();
+    var hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(diff / 1000 / 60);
+
+    // return (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
+    return { hours: hours, minutes: minutes };
+}
+
 daySelect.onValueChange = function() {
     if (!selectedDays.includes(this.selectedValue)) {
         dayToTimes[this.selectedValue] = [];
@@ -50,7 +69,33 @@ daySelect.onValueChange = function() {
         var p = document.createElement('p');
         p.innerHTML = this.selectedValue;
         p.classList.add('dayDivText');
+        var timespan = document.createElement('span');
+        timespan.innerHTML = ' 0 hours and 0 minutes';
+        timespan.classList.add('timeDivTimeSpan');
+        p.append(timespan);
         dayDiv.appendChild(p);
+
+        var timesDiv = document.createElement('div');
+
+        function refreshTotalTime() {
+            var total = { hours: 0, minutes: 0 }
+            for (var div of timesDiv.children) {
+                var startInp = div.getElementsByTagName('input')[0];
+                var endInp = div.getElementsByTagName('input')[1];
+
+                if (startInp.value === '' || endInp.value === '') {
+                    break;
+                }
+
+                var timeForDiv = diff(startInp, endInp);
+                total.hours += timeForDiv.hours;
+                total.minutes += timeForDiv.minutes;
+            }
+            total.hours += parseInt(total.minutes / 60);
+            total.minutes %= 60;
+            timespan.innerHTML = ' ' + total.hours + " hours and " + total.minutes + ' minutes';
+        }
+
 
         var addTimeButton = document.createElement('button');
         addTimeButton.classList.add('dayDivButton');
@@ -59,25 +104,28 @@ daySelect.onValueChange = function() {
             var timeDiv = document.createElement('div');
             timeDiv.classList.add('timeDiv');
 
-            var inpStart = createInput('time', 'startTime');
+            var inpStart = createInput('time', 'startTime *');
             inpStart.div.classList.add('timeDivInput');
             inpStart.div.classList.add('left');
+            inpStart.input.oninput = refreshTotalTime;
 
             var sep = document.createElement('p');
             sep.innerHTML = 'to';
             sep.classList.add('timeDivSeparator');
 
-            var inpEnd = createInput('time', 'endTime');
+            var inpEnd = createInput('time', 'endTime *');
             inpEnd.div.classList.add('timeDivInput');
             inpEnd.div.classList.add('right');
+            inpEnd.input.oninput = refreshTotalTime;
 
             timeDiv.appendChild(inpStart.div);
             timeDiv.appendChild(sep);
             timeDiv.appendChild(inpEnd.div);
 
-            insertAfter(timeDiv, dayDiv);
+            timesDiv.appendChild(timeDiv);
         }
         dayDiv.appendChild(addTimeButton);
+        dayDiv.appendChild(timesDiv);
         dom('availabilityDiv').appendChild(dayDiv);
 
         selectedDays.push(this.selectedValue);
